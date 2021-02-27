f76a08f9e1b57a293502a343d457cd92e5da9cfd
mike591 <mikemach123@gmail.com>
2021-02-27 14:37:52 -0800

    remove extra files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="App">This is the adventure game!</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./main.scss";
+import App from "components/App";
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA2gTkBQzekuYqiTmSHBMYvo5RvZNiv4A",
+  authDomain: "adventuregame-4ef24.firebaseapp.com",
+  projectId: "adventuregame-4ef24",
+  storageBucket: "adventuregame-4ef24.appspot.com",
+  messagingSenderId: "786430803048",
+  appId: "1:786430803048:web:fffc45e9f91414627e0fdd",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
